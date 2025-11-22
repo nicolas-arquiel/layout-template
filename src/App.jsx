17376af5dafe7cf69cd062232858590f043322f3
Nicolas <arquiel.nicolas@gmail.com>
2025-11-22 13:08:36 -0300

    se inicia proytecto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 text-white p-8">
+      <h1 className="text-4xl font-bold underline">
+        ¡Tailwind CSS v4 funcionando!
+      </h1>
+      <p className="text-lg mt-4">
+        Si ves este texto en azul con tipografía grande, todo está correcto.
+      </p>
+    </div>
+  )
+}
+
+export default App
